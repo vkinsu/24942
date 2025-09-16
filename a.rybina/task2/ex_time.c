0b76140da141aae5a2da74649fb1fa079ea70f8c
aneXXa <an_na_rib@mail.ru>
2025-09-16 13:22:44 +0700

    testing task 2

--- a.rybina/task2/ex_time.c
+++ a.rybina/task2/ex_time.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 extern char *tzname[];
 
@@ -9,12 +10,18 @@ int main(){
     time_t now;
     struct tm *sp;
 
+    // Set timezone to Pacific Standard Time (California)
+    // This works on UNIX systems including Solaris
+    setenv("TZ", "PST8PDT", 1);
+    tzset(); // Update timezone information
+
     (void) time( &now );
 
+    printf("Current time in California:\n");
     printf("%s", ctime( &now ) );
 
     sp = localtime(&now);
-    printf("%d/%d/%02d %d:%02d %s\n",
+    printf("Formatted: %d/%d/%02d %d:%02d %s\n",
         sp->tm_mon + 1, sp->tm_mday,
         sp->tm_year, sp->tm_hour,
         sp->tm_min, tzname[sp->tm_isdst]);
