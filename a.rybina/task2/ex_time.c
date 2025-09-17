01b486b5aa1303f02de5e1fd22d063322fd9cc57
aneXXa <an_na_rib@mail.ru>
2025-09-17 17:25:56 +0700

    edit3

--- a.rybina/task2/ex_time.c
+++ a.rybina/task2/ex_time.c
@@ -2,22 +2,41 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-#include <unistd.h>
-
 extern char *tzname[];
 
-int main(){
+main() {
     time_t now;
-
-    // Set timezone to Pacific Standard Time (California)
-    // This works on UNIX systems including Solaris
-    setenv("TZ", "PST8PDT", 1);
-    tzset(); // Update timezone information
+    struct tm *sp;
 
     (void) time( &now );
 
-    printf("Current time in California:\n");
     printf("%s", ctime( &now ) );
 
-    return 0;
+    sp = localtime(&now);
+    printf("%d/%d/%02d %d:%02d %s\n",
+        sp->tm_mon + 1, sp->tm_mday,
+        sp->tm_year, sp->tm_hour,
+        sp->tm_min, tzname[sp->tm_isdst]);
+    exit(0);
 }
+// #include <sys/types.h>
+// #include <stdio.h>
+// #include <time.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+
+// extern char *tzname[];
+
+// int main(){
+//     time_t now;
+
+//     setenv("TZ", "PST8PDT", 1);
+//     tzset();
+
+//     (void) time( &now );
+
+//     printf("Current time in California:\n");
+//     printf("%s", ctime( &now ) );
+
+//     return 0;
+// }
