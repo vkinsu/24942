167936fed09a12266f6cb6e44783be9ca0b90279
aneXXa <an_na_rib@mail.ru>
2025-09-17 17:34:09 +0700

    edit4

--- a.rybina/task2/ex_time.c
+++ a.rybina/task2/ex_time.c
@@ -1,42 +1,21 @@
-#include <sys/types.h>
+#include <sys/types.h>git
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-extern char *tzname[];
+#include <unistd.h>
 
-main() {
+extern char *tzname[]; //stores the names of the time zones
+
+int main(){
     time_t now;
-    struct tm *sp;
 
-    (void) time( &now );
+    setenv("TZ", "PST8PDT", 1);
+    tzset();
 
+    (void) time(&now); //get current time
+
+    printf("Current time in California:\n");
     printf("%s", ctime( &now ) );
 
-    sp = localtime(&now);
-    printf("%d/%d/%02d %d:%02d %s\n",
-        sp->tm_mon + 1, sp->tm_mday,
-        sp->tm_year, sp->tm_hour,
-        sp->tm_min, tzname[sp->tm_isdst]);
-    exit(0);
+    return 0;
 }
-// #include <sys/types.h>
-// #include <stdio.h>
-// #include <time.h>
-// #include <stdlib.h>
-// #include <unistd.h>
-
-// extern char *tzname[];
-
-// int main(){
-//     time_t now;
-
-//     setenv("TZ", "PST8PDT", 1);
-//     tzset();
-
-//     (void) time( &now );
-
-//     printf("Current time in California:\n");
-//     printf("%s", ctime( &now ) );
-
-//     return 0;
-// }
